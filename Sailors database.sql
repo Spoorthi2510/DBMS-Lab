106843ea6c9240bfa8c9f1b0698261326d8effde
Spoorthi2510 <78786434+Spoorthi2510@users.noreply.github.com>
2023-02-08 12:30:10 +0530

    Add files via upload

---
+++ Sailors database.sql
@@ -0,0 +1,77 @@
+/* Creating a sailor database with three tables */
+CREATE DATABASE IF NOT EXISTS sailors_157;
+USE sailors_157;
+CREATE TABLE IF NOT EXISTS sailors (
+sid varchar(25) NOT NULL PRIMARY KEY,
+sname TEXT NOT NULL,
+rating INTEGER NOT NULL,
+age INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS boat (
+bid varchar(25) NOT NULL PRIMARY KEY,
+bname TEXT NOT NULL,
+color TEXT
+);
+
+CREATE TABLE IF NOT EXISTS rservers (
+sid varchar(25) NOT NULL,
+bid varchar(25) NOT NULL,
+s_date DATE,
+FOREIGN KEY(sid) REFERENCES sailors(sid) ON DELETE CASCADE,
+FOREIGN KEY(bid) REFERENCES boat(bid) ON DELETE CASCADE
+);
+
+INSERT INTO sailors VALUES
+("S111", "Albert", 5, 45),
+("S222", "Tom", 6, 39),
+("S333", "Bob", 8, 36),
+("S444", "Jack", 10, 28),
+("S555", "Michael", 10, 42);
+("S666","John",3,44);
+
+INSERT INTO boat VALUES
+("B111", "BillyStorm", "Black"),
+("B222", "Pilly", "Red"),
+("B333", "TrillyStorm", "Green"),
+("B444", "Willy", "White"),
+("B555", "Lilly", "Blue");
+
+INSERT INTO rservers VALUES
+("S111", "B444", "2020-04-05"),
+("S222", "B222", "2019-12-16"),
+("S333", "B111", "2020-05-14"),
+("S444", "B333", "2019-08-30"),
+("S555", "B555", "2021-01-01"),
+("S333","B222","2022-09-23"),
+("S111","B555","2022-01-04"),
+("S333","B555","2022-08-17"),
+("S222","B444","2021-12-12"),
+("S333","B333","2022-02-27"),
+("S444","B111","2022-03-09"),
+("S333","B444","2022-04-21"),
+("S111","B222","2022-03-19"),
+("S222","B555","2023-01-29"),
+("S444","B555","2022-12-30"),
+("S111","B333","2020-11-11"),
+("S666","B111","2021-10-25"),
+("S555","B444","2022-10-25"),
+("S666","B333","2020-05-09"),
+("S666","B222","2023-02-03"),
+("S666","B111","2020-10-10"),
+("S666","B555","2020-10-18");
+
+SELECT * FROM sailors;
+SELECT * FROM boat;
+SELECT * FROM rservers;
+
+//Queries 
+select color from boat where bid in (select bid from rservers where sid in (select sid from sailors where sname="Albert"));
+select distinct  sailors.sid from sailors,rservers where rating>=8 or (sailors.sid=rservers.sid and bid="B333");
+select sname from sailors where sid not in (select distinct sid from rservers where bid in (select bid from boat where bname like "%Storm%"))order by sname;
+select sname from sailors where not exists ((select bid from boat) except (select bid from rservers where rservers.sid=sailors.sid));
+select sname,age from sailors where (select max(age) from sailors)=age;
+select boat.bid, AVG(age) from boat,sailors,rservers where sailors.age>=40 and boat.bid=rservers.bid and sailors.sid=rservers.sid group by bid having count(distinct sailors.sid)>=5;
+create view view1 as select sname,rating from sailors order by rating; select * from view1;
+create view view2 as select sname from sailors where sid in(select sid from rservers where s_date="2020-11-11");
+create view view3 as select sname,color from sailors,boat,rservers where sailors.sid=rservers.sid and boat.bid=rservers.bid and rating=10;
